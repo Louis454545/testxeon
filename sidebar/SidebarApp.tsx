92050199a0a725e10504a44687988b5af645e86a
Louis <la.programmations@gmail.com>
2025-01-25 22:54:45 +0100

    refactor: update SidebarApp component to improve subscription form layout and button size

--- sidebar/SidebarApp.tsx
+++ sidebar/SidebarApp.tsx
@@ -7,11 +7,13 @@ export default function SidebarApp() {
   return (
     <div className="h-full dark">
       <div className="flex items-center justify-between space-x-4 absolute bottom-0 w-full p-4">
-      <div className="flex w-full max-w-sm items-center space-x-2">
+      <div className="flex items-center space-x-2 w-full">
+        <div className="flex-1">
         <Input type="email" placeholder="Email" />
-        <Button type="submit"><SendIcon className="align-middle" /></Button>
       </div>
+        <Button type="submit" size="icon"><SendIcon /></Button>
       </div>
+    </div>
     </div>
   )
 }
