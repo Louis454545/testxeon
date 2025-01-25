37c90677ad8b5226599fe5f5264e1fba9231c7ef
Louis <la.programmations@gmail.com>
2025-01-25 22:25:36 +0100

    refactor: update SidebarApp component to include a subscription form with email input and button

--- sidebar/SidebarApp.tsx
+++ sidebar/SidebarApp.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import './styles.css'
 
@@ -5,7 +6,10 @@ export default function SidebarApp() {
   return (
     <div className="h-full dark">
       <div className="flex items-center justify-between space-x-4 absolute bottom-0 w-full p-4">
-        <Input id="input-test" aria-label="Input Test" placeholder="Test input" />
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type="email" placeholder="Email" />
+        <Button type="submit">Subscribe</Button>
+      </div>
       </div>
     </div>
   )
