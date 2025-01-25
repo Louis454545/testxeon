70ced404f52bd9f87613ffda7f9097d1038b859a
Louis <la.programmations@gmail.com>
2025-01-25 21:53:40 +0100

    refactor: simplify SidebarApp component by removing Card and unused imports

--- sidebar/SidebarApp.tsx
+++ sidebar/SidebarApp.tsx
@@ -1,24 +1,12 @@
-import {Button} from '@/components/ui/button'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import {Label} from '@/components/ui/label'
-import {Switch} from '@/components/ui/switch'
 import { Input } from "@/components/ui/input"
 import './styles.css'
-import shadcnLogo from '../images/shadcn.svg'
 
 export default function SidebarApp() {
   return (
-    <Card className="h-full dark">
-      <CardContent className="flex items-center justify-between space-x-4 absolute bottom-0 w-full p-4">
+    <div className="h-full dark">
+      <div className="flex items-center justify-between space-x-4 absolute bottom-0 w-full p-4">
         <Input id="input-test" aria-label="Input Test" placeholder="Test input" />
-      </CardContent>
-    </Card>
+      </div>
+    </div>
   )
 }
